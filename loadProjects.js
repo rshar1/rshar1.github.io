2ae529fc395b4d0544cbf843ce5af6a6c8deef9d
Raami Sharif <raamisharif@gmail.com>
2018-01-24 15:05:37 -0500

    changed the project background colors

--- loadProjects.js
+++ loadProjects.js
@@ -73,10 +73,41 @@ var getModalHtml = function(projectObj) {
 var getRandomColor = function() {
     var colors = 
     [
-        "green", 
-        "rgb(89, 123, 126)",
-        "orange", 
-        "yellow"
+            "#00ffff",
+            "#0000ff",
+            "#a52a2a",
+            "#00008b",
+            "#008b8b",
+            "#a9a9a9",
+            "#006400",
+            "#bdb76b",
+            "#8b008b",
+            "#556b2f",
+            "#ff8c00",
+            "#9932cc",
+            "#8b0000",
+            "#e9967a",
+            "#9400d3",
+            "#ff00ff",
+            "#ffd700",
+            "#008000",
+            "#4b0082",
+            "#f0e68c",
+            "#add8e6",
+            "#90ee90",
+            "#d3d3d3",
+            "#ffb6c1",
+            "#ffffe0",
+            "#00ff00",
+            "#ff00ff",
+            "#800000",
+            "#808000",
+            "#ffa500",
+            "#ffc0cb",
+            "#800080",
+            "#ff0000",
+            "#c0c0c0",
+            "#ffff00"
     ]
 
     return colors[Math.floor((Math.random() * colors.length))]
